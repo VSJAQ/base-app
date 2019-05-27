8f6833f8a9b9568b0c307040cef192aff9e92fe5
VSJAQ <51055013+VSJAQ@users.noreply.github.com>
2019-05-28 02:37:54 +0500

    adding

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Pikcha extends React.Component {
+    render() {
+        return (
+            <div className="Telo">
+                <div className="Header">
+                <img class="first" src={"https://media.wired.com/photos/5a970eb4927dc94e67685b0e/master/pass/matterhorn-802950172.jpg"} alt="Аватар" />
+                <h1>Images</h1>
+                </div>
+            <div className="Section">
+            <div className="Section-head">
+            <h2>Mountains</h2>
+            <button className="Button">delete</button>
+            </div>
+            <img class="Second" src={"https://cdn-image.travelandleisure.com/sites/default/files/styles/1600x1000/public/1507062474/hotel-everest-namche-nepal-mountain-lodge-MOUNTAIN1017.jpg?itok=g-S4SL9n"} alt="Картинка" />
+            </div>
+        </div>
+        )
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+ReactDOM.render(
+<Pikcha/>,
+    document.getElementById('root')
+);
